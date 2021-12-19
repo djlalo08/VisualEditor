ee3dc9c00632b50e674cb71c058089fd363604e2
David Lalo <djlalo08@gmail.com>
2021-12-19 01:11:06 -0500

    Forgot to include these with last review

--- ObjectHierarchy/Object.py
+++ ObjectHierarchy/Object.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing_extensions import Self
 from Point import Point
 import Canvas as C
 import ObjectHierarchy.ObjectReference as OR
@@ -10,8 +11,8 @@ class Object:
             pos: Point =Point(0,0), 
             offset: Point =Point(0,0), 
             single_point_position: bool = False,
-            children: list[OR.ObjectReference]= None, 
-            parent: OR.ObjectReference = None, constrained_to_parent: bool = False, offset_off_parent: Point = Point(0,0),
+            children_refs: list[OR.ObjectReference]= None, 
+            parent_ref: OR.ObjectReference = None, constrained_to_parent: bool = False, offset_off_parent: Point = Point(0,0),
             width: int = 0, height: int =0, 
             **_
     ) -> None:
@@ -20,11 +21,10 @@ class Object:
         self.offset: Point = offset
 
         self.single_point_position = single_point_position
-        self.x: str = "10"
 
-        self.children : list[OR.ObjectReference] = children if children is not None else []
+        self.children_refs : list[OR.ObjectReference] = children_refs if children_refs is not None else []
 
-        self.parent : OR.ObjectReference = parent
+        self.parent_ref : OR.ObjectReference = parent_ref
         self.constrained_to_parent = constrained_to_parent
         self.offset_off_parent = offset_off_parent
 
@@ -35,17 +35,14 @@ class Object:
         C.Canvas.id_map[self.id] = self
 
     def move(self, delta):
-        if self.parent is not None and self.constrained_to_parent:
-            self.parent.move(delta)
+        if self.parent_ref is not None and self.constrained_to_parent:
+            self.parent_ref.obj.move(delta)
         else:
             self.pos += delta
             self.update()
     
     def abs_pos(self):
         return self.pos + self.offset + self.offset_off_parent
-
-    def __invert__(self):
-        return self.abs_pos()
     
     def build_obj(self):
         raise NotImplementedError("build_obj must overridden but is not")
@@ -56,21 +53,25 @@ class Object:
             newPos = self.abs_pos().unpack()
             
         C.Canvas.canvas.coords(self.id, newPos)
-        for child in self.children:
-            child.offset = self.abs_pos()
-            child.update()
+        for child_ref in self.children_refs:
+            child_ref.obj.offset = self.abs_pos()
+            child_ref.obj.update()
             
     def __repr__(self) -> str:
         return self.id
     
     def prep_for_save(self):
-        if self.parent:
-            self.parent = self.parent.id
-        if self.children:
-            self.children = list(map(lambda c: c.id, self.children))
+        pass
         
     def prep_from_save_for_use(self, canvas, id_map):
-        if self.parent:
-            self.parent = id_map[self.parent]
-        if self.children:
-            self.children = list(map(lambda c_id: id_map[c_id], self.children))
+        pass
+            
+    def to_ref(self) -> OR.ObjectReference[Self]:
+        return OR.ObjectReference(self.id)
+
+    def __invert__(self) -> OR.ObjectReference[Self]:
+        return self.to_ref()
+    
+    @property
+    def ref(self) -> OR.ObjectReference[Self]:
+        return self.to_ref()
--- ObjectHierarchy/ObjectReference.py
+++ ObjectHierarchy/ObjectReference.py
@@ -1,34 +1,21 @@
 from __future__ import annotations
+from typing import Generic, TypeVar
 import Canvas as C
 import ObjectHierarchy.Object as O
 
-class ObjectReference:
-    def __init__(self, reference: int) -> None:
-        self.reference = reference
+T = TypeVar('T')
 
-    def move(self, delta):
-        self.get_obj().move(delta)
-    
-    def abs_pos(self):
-        self.get_obj().abs_pos()
+class ObjectReference(Generic[T]):
+    def __init__(self, id: int) -> None:
+        self.id = id
 
-    def __invert__(self):
-        return self.get_obj().__invert__()
+    def get_obj(self) -> T:
+        return C.Canvas.id_map[self.id]
     
-    def build_obj(self):
-        return self.get_obj().build_obj()
+    def __invert__(self) -> T:
+        return self.get_obj()
     
-    def update(self):
-        self.get_obj().update()
-            
-    def __repr__(self) -> str:
-        return self.get_obj().__repr__()
+    @property
+    def obj(self) -> T:
+        return self.get_obj()
     
-    def prep_for_save(self):
-        return self.get_obj().prep_for_save()
-        
-    def prep_from_save_for_use(self, canvas, id_map):
-        return self.get_obj().prep_from_save_for_use(canvas, id_map)
-
-    def get_obj(self) -> O.Object:
-        return C.Canvas.id_map[self.reference]
