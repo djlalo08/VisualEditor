0cabe98dbc35efd23c09d33d592a9a4bc6870044
David Lalo <djlalo08@gmail.com>
2022-04-10 19:47:35 -0400

    Fixing a navigation bug

--- Bindings/Navigator.py
+++ Bindings/Navigator.py
@@ -3,19 +3,22 @@ from abc import abstractmethod
 from numpy import empty
 from Canvas import Canvas
 from ObjectHierarchy.ObjectReference import ObjectReference
-from ObjectHierarchy.Selectable import Selectable, is_selectable
+from ObjectHierarchy.Selectable import is_selectable
 from Bindings.Selector import Selector
-from Utils import Stream, empty_if_null
+from Utils import Stream, get_obj_index
 
 class Navigator:
     @abstractmethod
     def move_selection(self): 
         raise NotImplemented
     
-
 def extract_selectable(ls):
     return Stream(ls).map(ObjectReference.get_obj).filter(is_selectable).to_list()
 
+def get_siblings(obj):
+    return (Canvas.selected 
+        and Canvas.selected.parent_ref 
+        and Canvas.selected.parent_ref.obj.children_refs)
 
 class NavigateDeeper(Navigator):
     def move_selection(self):
@@ -27,17 +30,16 @@ class NavigateDeeper(Navigator):
     
 class NavigateHigher(Navigator):
     def move_selection(self):
-        parent = Canvas.selected and Canvas.selected.parent_ref
-        if parent:
-            Selector().select_item(parent.obj)
+        parent_ref = Canvas.selected and Canvas.selected.parent_ref
+        if parent_ref:
+            Selector().select_item(parent_ref.obj)
+            new_index = get_obj_index(get_siblings(parent_ref.obj), parent_ref)
+            if new_index != -1:
+                Canvas.selected_index = new_index
 
 class NavigateLeftRight(Navigator):
     def move_selection(self):
-        sibling_refs = (Canvas.selected 
-            and Canvas.selected.parent_ref 
-            and Canvas.selected.parent_ref.obj.children_refs)
-
-        selectable_siblings = extract_selectable(sibling_refs)
+        selectable_siblings = extract_selectable(get_siblings(Canvas.selected))
         if selectable_siblings:
             old_selection = Canvas.selected_index
             Canvas.selected_index = self.update_index(selectable_siblings)
