f44398966507bcac7d05b9c292350c9c6787016a
David Lalo <djlalo08@gmail.com>
2022-04-25 16:19:51 -0400

    Making window close close app

--- interfaceApp.py
+++ interfaceApp.py
@@ -2,13 +2,7 @@ import tkinter as tk
 from Interface.InterfaceModal import InterfaceModal
 from LabelModal import LabelModal
     
-class InterfaceApp(tk.Frame):
-
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-        InterfaceModal(parent)
-
 if __name__ == "__main__":
     root = tk.Tk()
-    InterfaceApp(root).pack(fill="both", expand=True)
+    InterfaceModal(root)
     root.mainloop()
