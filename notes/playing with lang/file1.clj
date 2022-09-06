5e99e357a7714b4d19e7380e96e5a113e2e5f780
David Lalo <djlalo08@gmail.com>
2022-09-06 05:02:39 -0400

    Completed max-min-avg

--- notes/playing with lang/file1.clj
+++ notes/playing with lang/file1.clj
@@ -9,11 +9,11 @@
 (defn output [name value]
   (def outsmap (assoc outsmap name value)))
 
-(defn make-map [map-name [& inputs] [& outputs]] 
-  (let [results (apply map-name inputs)] 
+(defn make-map [map-name inputs outputs] 
+  (let [results (apply map-name inputs)]
     (doseq [[a b] (map vector outputs results)]
-      (output a b)
-     ((vector [results]) 0))))
+      (output a b))
+    (identity results)))
 
 (defn min-max [ls]
   (vector (apply min ls) (apply max ls)))
@@ -56,10 +56,13 @@
 
 (println "c " outsmap)
 
-(defx
+(println (defx
   [[:i0 :i1] [[1,2,3,4,5] "hi"]]
-  [(make-map min-max [(outsmap :i0)] [:o0 :o1])]
-  [:o0 :o1])
+  [(make-map min-max [(outsmap :i0)] [:o0 :o1])
+   (make-map div [(make-map add [(outsmap :o0) (outsmap :o1)] []) [2]] [:o2])]
+  [:o0 :o2 :o1]))
+
+(println "sum " (make-map add [1 5] []))
 
 (println "mcrexp "
  (macroexpand
