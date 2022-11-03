149374345883a4be3ac4ec0d0836a96f01373695
David Lalo <djlalo08@gmail.com>
2022-11-03 17:12:02 -0400

    Created to-do list and CSS for selected files

--- translator/clojure_to_jsx_.py
+++ translator/clojure_to_jsx_.py
@@ -1,8 +1,8 @@
 from translator.clj_example import clojure_code
 from translator.clojure_to_jsx.Annotater import annotate
-from translator.clojure_to_jsx.Jsxify import to_jsx_, to_jsx
-from translator.clojure_to_jsx.Tagify import to_tag
+from translator.clojure_to_jsx.Jsxify import to_jsx
 from translator.clojure_to_jsx.parser import parse
+from translator.clojure_to_jsx.Tagify import to_tag
 
 if __name__ == '__main__':
 
@@ -10,10 +10,10 @@ if __name__ == '__main__':
     # print('parsed\n', parsed)
 
     annotated = annotate(parsed)
-    # print('annotated\n', annotated)
+    print('annotated\n', annotated)
 
     tagged = to_tag(parsed)
-    # print('tagged\n', tagged)
+    print('tagged\n', tagged)
 
     jsx = to_jsx(tagged)
     print(jsx)
