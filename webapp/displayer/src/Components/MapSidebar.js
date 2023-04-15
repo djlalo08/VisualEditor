e48ba165e10d64f72e531fbfe582f13883ac159d
David Lalo <djlalo08@gmail.com>
2023-04-14 22:30:26 -0400

    Map can now determine their 'fix' in sidebar

--- webapp/displayer/src/Components/MapSidebar.js
+++ webapp/displayer/src/Components/MapSidebar.js
@@ -3,6 +3,9 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { updateAST } from '../Actions';
+import { addAttr, delAttr } from '../Utils/NodeUtils';
+
+const positions = ['infix', 'postfix', 'underfix', 'prefix'];
 
 export function MapSidebar({attrs, node}){
 
@@ -23,28 +26,49 @@ let onChangeOutCount = e => {
         updateAST();
     }
 }
+
+let onChangeVisual = e => {
+    delAttr(node, 'prefix');
+    delAttr(node, 'infix');
+    delAttr(node, 'postfix');
+    delAttr(node, 'underfix');
+    addAttr(node, e.target.value, 't');
+    updateAST();
+}
     
     
 let insCount = ins.children.length;
 let outsCount = outs.children.length;
 
+let position = 'underfix';
+for (let p of positions){
+    if (p in attrs){
+        position = p;
+        break;
+    }
+}
+
 return <>
     <Row>
-      <Form.Label column htmlFor="exampleColorInput">Ins</Form.Label>
+      <Form.Label column>Ins</Form.Label>
       <Col><Form.Control type="number" value={insCount} 
             onChange={onChangeInCount}/></Col>
     </Row>
     <Row>
-      <Form.Label column htmlFor="exampleColorInput">Outs</Form.Label>
+      <Form.Label column>Outs</Form.Label>
       <Col><Form.Control type="number" value={outsCount} 
             onChange={onChangeOutCount}/></Col>
     </Row>
     <br/>
     <br/>
     <Row>
-      <Form.Label column htmlFor="exampleColorInput">Visual</Form.Label>
-      <Col><Form.Control type="number" value={outsCount} 
-            onChange={onChangeOutCount}/></Col>
+        <Form.Label column>Visual</Form.Label>
+        <Col> <Form.Select value={position} onChange={onChangeVisual}>
+            <option value='underfix'>Underfix</option>
+            <option value='prefix'>Prefix</option>
+            <option value='infix'>Infix</option>
+            <option value='postfix'>Postfix</option>
+        </Form.Select> </Col>
     </Row>
 </>;
 
