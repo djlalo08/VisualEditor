277067a6e62086b335b1e7137086c1fae0e3cacc
David Lalo <djlalo08@gmail.com>
2023-08-08 00:55:09 -0400

    Using sleep to make tests work
    Tried doing all kinds of async and callbacks, but it kept making a mess and breaking things. Eventually just made a small wait between each call...

--- webapp/displayer/src/Utils/Tests.js
+++ webapp/displayer/src/Utils/Tests.js
@@ -5,7 +5,25 @@ export async function runTests(app){
     await assertList(app, 'lambdas', 11, [8,9,10,11,12]);
     await assertValue(app, 'lambdas', 4, 2);
     await assertValue(app, 'inc_test', 2, 6);
+    await assertNestedList(app, 'lambda_nest', 28, [[3,4,5], [3,4,5], [3,4,5]]);
 
+}
+
+async function assertNestedList(app, fileName, id, expectedList){
+    let assertFn = actualValue => {
+        if (expectedList.every(
+                (ls, i) => ls.every( 
+                    (val, j) => 
+                        val == actualValue[i][j]
+                ))
+        ){
+            console.log(`Correct value of id ${id} at ${fileName}`);
+        } else {
+            console.log(`Wrong value at '${fileName}'! Expected ${expectedList} at id ${id} but got:`);
+            console.log(actualValue);
+        }
+    }
+    await runTest(app, fileName, id, assertFn);  
 }
 
 async function assertList(app, fileName, id, expectedList){
@@ -33,5 +51,13 @@ async function assertValue(app, fileName, id, expectedValue){
 }
 
 async function runTest(app, fileName, id, assertFn){
-    await openFile(fileName, () => setSelectedById(id, () => app.eval_(assertFn)));
+    openFile(fileName);
+    await sleep(100);
+    setSelectedById(id);
+    await sleep(1);
+    app.eval_(assertFn);
+}
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
