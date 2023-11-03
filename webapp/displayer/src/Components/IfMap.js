6a47bba6310e5dcfb9e0caf428b1fe972244df1f
David Lalo <djlalo08@gmail.com>
2023-11-03 02:17:39 -0400

    Playing around with if/else to make it nicer

--- webapp/displayer/src/Components/IfMap.js
+++ webapp/displayer/src/Components/IfMap.js
@@ -1,22 +1,39 @@
-import React from "react";
 import { addProps } from "../Utils/ReactUtils";
 
 export function IfMap(props){
     let {id, className, ins, other} = props;
     let newIns = addProps(ins, {prefix:'t'}) ;
-    return (<table id={id} className={className} {...other}><tbody>
-        <tr>
-            <td>if</td>
-            <td>{addProps(newIns, {onlyShowIdx:0, name:'if'})}</td>
-        </tr>
-        <tr>
-            <td>then</td>
-            <td>{addProps(newIns, {onlyShowIdx:1})}</td>
-        </tr>
-        <tr>
-            <td>else</td>
-            <td>{addProps(newIns, {onlyShowIdx:2})}</td>
-        </tr>
-    </tbody></table>);
-    
+    return (<div id={id} className={className} {...other}>
+        <span>
+
+        <table style={
+            {  display: 'block',
+            'margin-left': 'auto',
+            'margin-right': 'auto',
+            width: '5%'}}>
+            <tr>
+                <td>
+                    if
+                </td>
+                <td>
+                {addProps(newIns, {onlyShowIdx:0, name:'if'})}
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td> then </td>
+                <td> {addProps(newIns, {onlyShowIdx:1})} </td>
+                <td style={{width: '12px'}}></td>
+                <td style={{width: '1px', backgroundColor: 'gray'}}></td>
+                <td style={{width: '25px'}}></td>
+                <td> else </td>
+                <td> {addProps(newIns, {onlyShowIdx:2})} </td>
+
+            </tr>
+
+        </table>
+        </span>
+
+    </div>)
 }
