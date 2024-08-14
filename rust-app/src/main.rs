8b9fb7cfba944a980bb225bbe183ba7226b62a1d
David Lalo <djlalo08@gmail.com>
2024-08-14 19:00:14 -0400

    Removing demo stuff. Adding in webapp's CSS

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -18,18 +18,5 @@ fn App() -> Element {
     rsx! {
         link { rel: "stylesheet", href: "main.css" }
         link { rel: "stylesheet", href: "bootstrap-5.3.3-dist/css/bootstrap.css" }
-        img { src: "header.svg", id: "header" }
-        div { id: "links",
-            a { href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
-            a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-            a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-            a { href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
-            a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
-                "💫 VSCode Extension"
-            }
-            a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-        }
-        button { class:"btn btn-primary", "Button text "}
-        // button { class:"btn btn-lg btn-danger", data_bs_toggle:"popover", data_bs_title:"Popover title", data_bs_content:"And here's some amazing content. It's very engaging. Right?", "Click to toggle popover" }
     }
 }
